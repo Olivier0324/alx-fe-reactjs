889ecf4699143c8afef62f70e4f11ed90b123030
Olivier0324 <techies.dev24@gmail.com>
2025-07-20 21:55:49 +0200

    Created my first project with navigation

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <footer style={{
+      backgroundColor: '#f1f1f1',
+      padding: '20px',
+      textAlign: 'center',
+      marginTop: '20px'
+    }}>
+      <p>© 2025 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
--- my-company/src/main.jsx
+++ my-company/src/main.jsx
@@ -1,33 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './Home'
-import About from './About'
-import Contact from './Contact'
-import Services from './Services'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element:<Home />
-  },
-  {
-    path: '/about',
-    element:<About />
-  },
-  {
-    path: '/services',
-    element:<Services />
-  },
-  {
-    path: '/contact',
-    element:<Contact />
-  }
-  
-])
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
