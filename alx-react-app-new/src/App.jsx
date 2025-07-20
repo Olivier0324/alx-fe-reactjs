57da0165cd59b5f00ffd3bc5828a5d5d11921697
Olivier0324 <techies.dev24@gmail.com>
2025-07-20 19:18:49 +0200

    Feat: Created counter App by using react Hooks

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,7 +7,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
-import Card from './components/card'
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -39,6 +39,7 @@ function App() {
       <MainContent />
       <Footer />
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <Counter />
     </>
   )
 }
