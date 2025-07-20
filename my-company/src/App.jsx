b0cbf8344b993b7486fd00e15cd821b9ab1f51da
Olivier0324 <techies.dev24@gmail.com>
2025-07-20 22:18:30 +0200

    Created my first project with navigation

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -3,22 +3,18 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
 import Contact from './pages/Contact';
-import Footer from './components/Footer'; // Optional
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar'; // ✅ Make sure this path matches
 
 function App() {
   return (
     <BrowserRouter>
-    <Navbar />
-      <div style={{ minHeight: '80vh' }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-      <Footer /> {/* Optional */}
+      <Navbar /> {/* ✅ Include Navbar here */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </BrowserRouter>
   );
 }
