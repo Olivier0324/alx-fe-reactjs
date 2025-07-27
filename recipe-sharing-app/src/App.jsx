28dcba5d915243ba518886150c8c50b113a4b1b0
Olivier0324 <techies.dev24@gmail.com>
2025-07-27 15:41:43 +0200

    feat: done with Recipe Sharing App with Zustand task 1

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,27 +1,27 @@
-import { Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link, useParams } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
 
-function App() {
-  return (
-    <div>
-      <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
-      <Routes>
-        <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
-      </Routes>
-    </div>
-  );
-}
-
-// Wrapper to extract the recipe ID from the URL param and pass it as a prop
-import { useParams } from 'react-router-dom';
-const RecipeDetailsWrapper = () => {
+function RecipeDetailsWrapper() {
   const { id } = useParams();
   const recipeId = Number(id);
   return <RecipeDetails recipeId={recipeId} />;
-};
+}
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <h1>Recipe Sharing App</h1>
+        <AddRecipeForm />
+        <RecipeList />
+        <Routes>
+          <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
 
 export default App;
