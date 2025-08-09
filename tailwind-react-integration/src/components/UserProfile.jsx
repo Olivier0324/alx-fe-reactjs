f3c41b9dc536d530484b06d0a49d6cea4e579a8f
Olivier0324 <techies.dev24@gmail.com>
2025-08-09 17:25:21 +0200

    Feat: Added responsiveness

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,10 +1,10 @@
 
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 p-8 max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-      <img src="https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D" alt="User"  className="rounded-full w-36 h-36 object-cover mx-auto"/>
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+    <div className="user-profile bg-gray-100 p-4 md:p-8 max-w-xs md:max-w-sm  mx-auto my-20 rounded-lg shadow-lg">
+      <img src="https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D" alt="User"  className="rounded-full w-24 h-24 object-cover mx-auto  md:w-36 md:h-36 "/>
+      <h1 className="text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
+      <p className="text-gray-600 text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   );
 }
